50b497109dba6ac40bfcbd290a25c82f9cef0117
david <djharper2000@gmail.com>
2025-04-13 11:03:45 -0500

    Complex Number support and saving data

--- parallel_lapack_solver.c
+++ parallel_lapack_solver.c
@@ -1,44 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <complex.h>
 #include <omp.h>
 
-// LAPACK prototype for dgesv (solves Ax = b)
-extern void dgesv_(int *n, int *nrhs, double *a, int *lda,
-                   int *ipiv, double *b, int *ldb, int *info);
+// LAPACK complex solver
+extern void zgesv_(int*, int*, double _Complex*, int*, int*,
+                   double _Complex*, int*, int*);
 
-void build_matrix_and_rhs(int size, double *A, double *b, int seed) {
-    srand(seed);
-    for (int i = 0; i < size * size; ++i)
-        A[i] = 1.0 + rand() % 10;  // simple random matrix
+// Simulate DDA matrix and incident field
+void generate_complex_A_and_E(int N, double freq,
+                              double _Complex *A, double _Complex *E_inc) {
+    for (int i = 0; i < N * N; ++i) {
+        A[i] = (i % (N + 1) == 0) ? (1.0 + freq) + 0.1*I : (0.01 * freq) + 0.02*I;
+    }
 
-    for (int i = 0; i < size; ++i)
-        b[i] = 1.0 + rand() % 10;  // random RHS
+    for (int i = 0; i < N; ++i) {
+        E_inc[i] = 1.0 + 0.0*I;  // Real incident field
+    }
+}
+
+// Solve and write polarization vector p to a file
+void solve_complex_system(int N, double freq) {
+    double _Complex *A = malloc(N * N * sizeof(double _Complex));
+    double _Complex *E_inc = malloc(N * sizeof(double _Complex));
+    int *ipiv = malloc(N * sizeof(int));
+    int nrhs = 1, info;
+
+    generate_complex_A_and_E(N, freq, A, E_inc);
+
+    zgesv_(&N, &nrhs, A, &N, ipiv, E_inc, &N, &info);
+
+    if (info == 0) {
+        char filename[64];
+        snprintf(filename, sizeof(filename), "output_freq_%.2f.txt", freq);
+        FILE *fp = fopen(filename, "w");
+        if (fp) {
+            for (int i = 0; i < N; ++i)
+                fprintf(fp, "%g %g\n", creal(E_inc[i]), cimag(E_inc[i]));
+            fclose(fp);
+            printf("freq=%.2f: wrote to %s\n", freq, filename);
+        } else {
+            printf("freq=%.2f: failed to open file\n", freq);
+        }
+    } else {
+        printf("freq=%.2f: Solve failed (info=%d)\n", freq, info);
+    }
+
+    free(A); free(E_inc); free(ipiv);
 }
 
 int main() {
-    const int num_systems = 8;   // how many independent systems to solve
-    const int size = 4;          // dimension of each system
+    const int N = 100;  // Number of dipoles
+    const int num_freqs = 10;
+    const double f_start = 0.1, f_end = 1.0;
 
     #pragma omp parallel for
-    for (int i = 0; i < num_systems; ++i) {
-        double A[size * size];
-        double b[size];
-        int ipiv[size];
-        int info;
-        int nrhs = 1;
-
-        build_matrix_and_rhs(size, A, b, i + 1);
-
-        dgesv_((int *)&size, &nrhs, A, (int *)&size, ipiv, b, (int *)&size, &info);
-
-        if (info == 0) {
-            printf("Thread %d: Solution x = [", omp_get_thread_num());
-            for (int j = 0; j < size; ++j)
-                printf(" %g", b[j]);
-            printf(" ]\n");
-        } else {
-            printf("Thread %d: Error solving system (info=%d)\n", omp_get_thread_num(), info);
-        }
+    for (int i = 0; i < num_freqs; ++i) {
+        double freq = f_start + i * (f_end - f_start) / (num_freqs - 1);
+        solve_complex_system(N, freq);
     }
 
     return 0;
