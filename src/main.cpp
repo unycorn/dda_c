eb4e61c0ac2d72dafb49035950d00c25db818ac1
david <djharper2000@gmail.com>
2025-04-22 17:34:37 -0500

    seperate ordered for disorderd slurm

--- src/main.cpp
+++ src/main.cpp
@@ -139,7 +139,7 @@ int main(int argc, char* argv[]) {
     generate_disordered_positions(positions.data(), N_width, N_height, spacing, disorder, seed);
 
     run_simulation(100e12, 500e12, 30, positions, N, spacing, disorder, seed);
-    run_simulation(201e12, 299e12, 20, positions, N, spacing, disorder, seed);
+    run_simulation(151e12, 251e12, 20, positions, N, spacing, disorder, seed);
 
     return 0;
 }
