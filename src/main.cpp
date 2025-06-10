129df60ee7f95836a49a0bd3d92480857d3b9520
david <djharper2000@gmail.com>
2025-06-10 13:25:30 -0500

    we'll see...

--- src/main.cpp
+++ src/main.cpp
@@ -103,8 +103,7 @@ void run_simulation(
                 }
             }
             
-            std::cout << "\nTransposed Interaction Matrix:\n";
-            print_complex_matrix(A_transposed.data(), 2 * N, 2 * N);
+            print_complex_matrix("Transposed Interaction Matrix", A_transposed.data(), 2 * N);
 
             cudaMemcpy(A_device, A_transposed.data(), sizeof(cuDoubleComplex) * 2 * N * 2 * N, cudaMemcpyHostToDevice);
             
