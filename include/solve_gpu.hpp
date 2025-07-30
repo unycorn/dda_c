61a5ae8e795b8370960f11ff4f6fdb59c2f36603
david <djharper2000@gmail.com>
2025-07-30 00:27:01 -0500

    solve for both polarizations of normally incident light at once

--- include/solve_gpu.hpp
+++ include/solve_gpu.hpp
@@ -4,4 +4,5 @@
 
 void init_cuda_device();
 void solve_gpu(cuDoubleComplex* A_device, cuDoubleComplex* b_host, int N);
+void solve_gpu_multiple_rhs(cuDoubleComplex* A_device, cuDoubleComplex* B_host, int N, int nrhs);
 void invert_6x6_matrix_lapack(cuDoubleComplex* matrix);
