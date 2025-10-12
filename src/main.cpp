b8c0be50cf0f21b502b7a130c13c0d63d7a8094b
david <djharper2000@gmail.com>
2025-10-11 23:34:40 -0500

    added periodicity capability in 1D (no lattice sum needed)

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,7 @@ void run_simulation(
 
         std::vector<std::complex<double>> A_host(2 * N * 2 * N, std::complex<double>(0.0, 0.0));
         // cuDoubleComplex* A_device = get_full_interaction_matrix_scalar(A_host.data(), positions.data(), alpha.data(), angles.data(), N, k);
-        cuDoubleComplex* A_device = get_full_interaction_matrix_scalar_1Dperiodic(A_host.data(), positions.data(), alpha.data(), angles.data(), N, k, vec3{1365.0e-9, 0.0, 0.0}, 200);
+        cuDoubleComplex* A_device = get_full_interaction_matrix_scalar_1Dperiodic(A_host.data(), positions.data(), alpha.data(), angles.data(), N, k, vec3{1365.0e-9, 0.0, 0.0}, 1000);
         std::cout << "freq " << freq << ": Finished Computing Interaction Matrix!\n";
 
         try {
