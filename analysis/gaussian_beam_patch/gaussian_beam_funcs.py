83c8c28b0a1831db3e706541bdc20da26a497ca9
david <djharper2000@gmail.com>
2025-11-11 23:29:32 -0600

    gaussian beam analysis

--- analysis/gaussian_beam_patch/gaussian_beam_funcs.py
+++ analysis/gaussian_beam_patch/gaussian_beam_funcs.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.constants import c, mu_0, epsilon_0, pi
 import read_polarizations
+import dipole
 # from numba import jit
 
 def gaussian_beam(x, y, z, w0, freq):
@@ -39,7 +40,17 @@ print(gaussian_beam_downward(0, 0, z, 5e-6, 300e12))
 def main():
     parser = argparse.ArgumentParser(description='Create animated Voronoi diagram of polarization data')
     parser.add_argument('csv_file', help='CSV file with x,y,z,theta columns')
+    parser.add_argument('distance', help='sampling distance')
+    parser.add_argument('width', help='width of sampling region')
+    parser.add_argument('density', help='number of sampling sites by length')
     args = parser.parse_args()
+
+    width = args.width
+    wover2 = width/2
+    z = -args.distance
+    sample_N = args.density
+
+    A = width**2 / sample_N**2
 
     # Read position data
     df = pd.read_csv(args.csv_file)
@@ -60,8 +71,38 @@ def main():
     
     # Sort by frequency
     data_pairs.sort()
+
+    sample_locations = []
+    for x in np.linspace(-wover2, wover2, sample_N):
+        for y in np.linspace(-wover2, wover2, sample_N):
+            sample_locations.append([x,y,z])
+
+    # Process each frequency
     for d in data_pairs:
-        print(d[0])
+        P0 = 0
+        Pt = 0
+
+        freq, file, N, polarizations = d
+        
+        for r_s in sample_locations:
+            Ex, By = gaussian_beam_downward(r_s[0], r_s[1], r_s[2], 5e-6, freq)
+            P0 += Ex * By / mu_0
+
+            Ey = Bx = 0
+            for i in range(N):
+                r_p = positions[i]
+                px, mz = polarizations[i]
+                
+                E, H = dipole.calculate_both_dipole_fields(r_p, r_s, [px, 0, 0, 0, 0, mz], 2*pi*freq)
+                Ex = Ex + E[0]
+                Ey = Ey + E[1]
+                Bx = Bx + mu_0 * H[0]
+                By = By + mu_0 * H[1]
+
+            Pt += (Ex * By - Ey * Bx) / mu_0
+        print(f"{freq*1e-12:.0f} THz", P0, Pt)
+            
+
 
 if __name__ == "__main__":
     main()
