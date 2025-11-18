bee3a56047c1864b5ccd64dc2eb2b38ee3494a90
david <djharper2000@gmail.com>
2025-11-17 23:26:40 -0600

    changed finite size patch params

--- analysis/AbsExtMeasure/AbsExtMeasure.py
+++ analysis/AbsExtMeasure/AbsExtMeasure.py
@@ -181,7 +181,8 @@ def calculate_absorption_extinction_jit(positions_array, polarizations_array, be
         EB_loc[4] += Binc_y
 
         E_loc = EB_loc[:3]
-        print(E_loc * (7.07712e-32 + 1.1978e-31j), px)
+        if r1_i == 0:
+            print(E_loc[0] * (7.07712e-32 + 1.1978e-31j), px)
         E_inc = np.array([Einc_x, 0.0 + 0.0j, 0.0 + 0.0j])
 
         # extinguished_power += np.pi * freq * np.imag(np.conj(E_inc[0]) * p_vec[0] + np.conj(E_inc[1]) * p_vec[1] + np.conj(E_inc[2]) * p_vec[2])
